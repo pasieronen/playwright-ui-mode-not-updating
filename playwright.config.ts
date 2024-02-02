585c171fc56cc266960fd26729f110b56a27925e
Pasi Eronen <pe@iki.fi>
2024-02-02 12:08:04 +0200

    add slowMo + couple of more actions to make it easier to see progress

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  use: {
+    launchOptions: {
+      slowMo: 1000
+    }
+  },
   projects: [
     {
       name: 'chromium',
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,4 +8,8 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Mock APIs' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Mock API requests' })).toBeVisible();
 });
